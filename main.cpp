fce556e47498c79b040f3f7f85b7901d881bb30a
Géza László Szeghy <szeghygl@gmail.com>
2022-03-12 20:34:20 +0100

    better code?, fixed fonts and added shuttle with vertical movement

--- main.cpp
+++ main.cpp
@@ -1,56 +1,147 @@
 #include <iostream>
 #include <graphics.hpp>
+#include <time.h>
+#include <math.h>
 using namespace std;
 using namespace genv;
 
-const int szelesseg = 800, magassag = 800;
+const int width = 800, height = 800;
+const double gravity = 5;
+
+
+struct Shuttle {
+
+    Shuttle(int x_, int y_): x(x_), y(y_) {}
+
+    void draw() {
+        gout << color(210, 210, 210) << move_to(x - 10, y - 10) << box(20, 20) << move_to(x - 10, y + 10)
+             << line_to(x - 15, y + 20) << move_to(x + 10, y + 10) << line_to(x + 15, y + 20);
+    }
+
+    void movey(double accel) {
+
+
+        if (y <= height - 35 && y >= 15)
+            y += (gravity * accel * fabs(accel));
+        else if (y >= height - 35) {
+            y = height - 35;
+            y += (gravity * accel * fabs(accel));
+        }
+        else if (y <= 16) {
+            y = 16;
+            y += (gravity * accel * fabs(accel));
+        }
+    }
+
+    void movex() {
+
+    }
+
+private:
+    int x, y;
+};
+
+
+void textpos(canvas& c, string s, int y, int size) {
+    c.load_font("LiberationSans-Regular.ttf", size);
+    int x = (width - gout.twidth(s)*(size/16.0) - 10)/2 - 1;
+    c << color(255, 255, 255) << move_to(x, y) << text(s);
+}
+
+void menu(event ev) {
+    canvas c;
+    string s;
+    c.open(800, 800);
+
+    textpos(c, "Moon-Lander", 99, 50);
+    textpos(c, "PLAY", 407, 30);
+    textpos(c, "Help: \"WASD\" to move", 499, 16);
+
+    // borders for the "PLAY" button
+    c << move_to(width/2-51, height/2-1) << line_to(width/2-51, height/2+49) << line_to(height/2+49, height/2+49)
+                                         << line_to(height/2+49, height/2-1) << line_to(width/2-51, height/2-1) << refresh;
+
+    gout << stamp(c, 0, 0) << refresh;
+
+    while (gin >> ev) {
+        if ((ev.pos_x >= 350 &&
+             ev.pos_x <= 450 &&
+             ev.pos_y >= 400 &&
+             ev.pos_y <= 450 &&
+             ev.button == btn_left) || ev.keycode == key_enter) {
+            gout << color(0, 0, 0) << move_to(0, 0) << box(width, height) << refresh;
+            break;
+
+        } else if (ev.type == ev_key)
+            if (ev.keycode == key_escape)
+                exit(0);
+    }
+}
+
+void gameloop(event ev) {
+    int y0 = rand() % (height - 500) + 50;
+    double accel = 0;
+    bool gameover = false;
+
+    Shuttle shuttle(rand() % (width - 200) + 100, y0);
+
+    while (gin >> ev) {
+
+        accel += (1.0/50.0);
+
+        if (accel >= 1) {
+            accel = 1;
+        }
+
+        if (ev.type == ev_timer){
+
+            gout << color(0, 0, 0) << move_to(0, 0) << box(width, height);
+
+            shuttle.draw();
+            shuttle.movey(accel);
+
+        } else if (ev.type == ev_key){
+            if (ev.keycode == 'w') {
+                accel -= (5.0/50.0);
+                shuttle.movey(accel);
+            } else if (ev.keycode == 'a')
+                shuttle.movex();
+             else if (ev.keycode == 'd')
+                shuttle.movex();
+        }
+
+        if (ev.type == ev_key)
+            if (ev.keycode == key_escape)
+                exit(0);
+
+        if (gameover)
+            break;
+
+        gout << refresh;
+    }
+}
+
+void gameover(event ev) {
+
+}
 
 int main()
 {
-    gout.open(szelesseg, magassag);
+    srand(time(NULL));
+
+    gout.open(width, height);
     gout << refresh;
 
-    canvas c;
-    c.open(800, 800);
-    c.load_font("C:\\Users\\szgeza\\Documents\\GraphicsKitQt\\LiberationSans-Regular.ttf", 50);
-    c << color(255, 255, 255) << move_to(250, 150) << text("Moon-Lander");
-    c.load_font("C:\\Users\\szgeza\\Documents\\GraphicsKitQt\\LiberationSans-Regular.ttf", 30);
-    c << color(255, 255, 255) << move_to(364, 408) << text("PLAY");
-    c << move_to(350, 400) << line_to(350, 450) << line_to(450, 450) << line_to(450, 400) << line_to(350, 400);
-    c.load_font("C:\\Users\\szgeza\\Documents\\GraphicsKitQt\\LiberationSans-Regular.ttf", 16);
-    c << color(255, 255, 255) << move_to(340, 500) << text("\"WASD\" to move") << refresh;
-    gout << stamp(c, 0, 0) << refresh;
-
     event ev;
-    gin.timer(50);
-    while (gin >> ev) {
+    gin.timer(20);
 
-        if (ev.pos_x >= 350 && ev.pos_x <= 450 && ev.pos_y >= 400 && ev.pos_y <= 450 && ev.button == btn_left) {
-            cout << "o" << endl;
-            gout << color(0, 0, 0) << move_to(0, 0) << box(szelesseg, magassag) << refresh;
-            break;
-        }
-        else if (ev.type == ev_key)
-        {
-            if (ev.keycode == key_escape)
-            {
-                return 0;
-            }
-        }
-    }
+    menu(ev);
 
-    while (gin >> ev) {
-
-    if (ev.type == ev_timer){
-
-    }
-    else if (ev.type == ev_key){
-        if (ev.keycode == key_escape){
-            return 0;
-        }
-    }
-
+    while (ev.button != key_escape) {
+        gameloop(ev);
+        gameover(ev);
     }
 
     return 0;
+
 }
